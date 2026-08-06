80d6d46db2edbce3656cfaaa8747ec2190aefda8
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-06 19:30:57 +0600

    feat: implement enum values on user model

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,18 +1,33 @@
 import mongoose from 'mongoose';
 
 // create an user schema
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['Active', 'Inactive'],
+      default: 'Active',
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  
-});
+  { timestamps: true, versionKey: false },
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
