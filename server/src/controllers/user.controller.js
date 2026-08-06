ddc27fe3b925373c4bf9465dc75f7253a4303aa7
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-06 21:09:11 +0600

    feat: user count documents added in code

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,6 +1,14 @@
+import User from '../models/user.model.js';
+
 export const getStats = async (req, res) => {
   try {
-    // 
+    // coutns
+    const total = await User.countDocuments();
+    const active = await User.countDocuments({ status: 'Active' });
+    const inactive = await User.countDocuments({ status: 'Inactive' });
+
+    // response
+    res.json({ total, active, inactive });
   } catch (error) {
     res.status(500).json({
       success: false,
