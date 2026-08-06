a8f28fbf67b932911d520dae053dfc7b899cd96e
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-06 19:25:50 +0600

    feat: implement user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+// create an user schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  
+});
