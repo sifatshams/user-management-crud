d0068f6f7dfc027149bd31225bf1c6b2f036e69b
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-06 19:27:02 +0600

    fix: fix the app.js file code

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import userRoute from './routes/user.route.js';
 
 const app = express();
 
@@ -11,7 +12,9 @@ app.use(
     credentials: true,
   }),
 );
+app.use(express.urlencoded({ extended: true }));
 
 // api endpoint's
+app.use('/api/v1/users', userRoute);
 
 export default app;
