b2cbe78bb49d683f3acc70f864fea7537d218a4c
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-07 10:07:56 +0600

    feat: implemet createUser route logic

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {
+  createUser,
   getAllUsers,
   getStats,
   getUserById,
@@ -11,7 +12,8 @@ const userRoute = express.Router();
 // routes
 userRoute.get('/stats', getStats);
 userRoute.get('/search/:query', searchUser);
-userRoute.get('/:id', getUserById);
 userRoute.get('/', getAllUsers);
+userRoute.get('/:id', getUserById);
+userRoute.post('/', createUser);
 
 export default userRoute;
