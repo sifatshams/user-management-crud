b481e0a1774d5f926a24e4db10e0de0bf13e2966
Sifat Bin Anwar <sifatshams@outlook.com>
2026-08-07 09:03:12 +0600

    feat: added getUserById route

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getStats, searchUser } from '../controllers/user.controller.js';
+import {
+  getStats,
+  getUserById,
+  searchUser,
+} from '../controllers/user.controller.js';
 
 const userRoute = express.Router();
 
 // routes
 userRoute.get('/stats', getStats);
 userRoute.get('/search/:query', searchUser);
+userRoute.get('/:id', getUserById);
 
 export default userRoute;
